5aad30792e77f75e0865dc231ba2d324375d1b32
Jono <jono2713@gmail.com>
2019-12-24 16:50:22 +0000

    initial setup

---
+++ src/views/Desktop.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <Taskbar />
+  </div>
+</template>
+
+<script>
+import Taskbar from '@/components/Taskbar.vue';
+
+export default {
+  name: 'home',
+  components: {
+    Taskbar,
+  },
+};
+</script>
