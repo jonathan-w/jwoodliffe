64a76b9ff8c188772b31cf82202417b280eb1ead
Jono <jono2713@gmail.com>
2019-12-25 07:03:05 +0000

    scss vue config fixed with global imports

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,10 @@
 module.exports = {
   css: {
     loaderOptions: {
-      sass: {
-        // Make scss variables and mixins globally available for .vue components
-        data: `
-          @import "@/styles/_variables.scss";
-          @import "@/styles/_mixins.scss";
+      scss: {
+        prependData: `
+          @import "@/scss/_variables.scss";
+          @import "@/scss/_mixin.scss";
         `,
       },
     },
